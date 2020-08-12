47b6640cb14f38132e1b8717b3962e16a4d6decb
j3ffh95 <j3ffh95@gmail.com>
2020-08-12 12:14:55 -0400

    Fetch a random meal from api

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 // Global Variables
 const getMealBtn = document.querySelector("#get_meal");
 const mealContainer = document.querySelector("#meal");
+
+getMealBtn.addEventListener("click", () => {
+  fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+    .then((res) => res.json())
+    .then((res) => {
+      createMeal(res.meals[0]);
+    });
+});
